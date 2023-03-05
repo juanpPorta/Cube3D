40933876862d4d111d3b2b9eee323124b694eb5e
Brandom moore <brandommoore@gmail.com>
2023-03-05 23:55:27 +0100

    Indentifiers checker added

--- parser/utils/data_assigner.c
+++ parser/utils/data_assigner.c
@@ -12,24 +12,31 @@
 
 #include "../parser.h"
 
-// /* Check if identifier is equal
-// */
-// int		check_identifier(char *identifier)
-// {
-// 	if (identifier != "NO" || identifier != "EA" || identifier != "WE" ||
-// 		identifier != "SO" || identifier != "F" || identifier != "C")
-// 			return (0);
-// 	return (1);
-// }
-
+/* Check if identifier is equal
+*/
+int	check_identifier(char *identifier)
+{
+	printf("Identifier --> %s\n", identifier);
+	if (ft_strncmp(identifier, "NO", 2) != 0
+		&& ft_strncmp(identifier, "EA", 2) != 0
+		&& ft_strncmp(identifier, "WE", 2) != 0
+		&& ft_strncmp(identifier, "SO", 2) != 0
+		&& ft_strncmp(identifier, "F", 1) != 0
+		&& ft_strncmp(identifier, "C", 1) != 0)
+	{
+		printf("Bad sprite/color indentifier in map. Check it\n");
+	}
+	return (1);
+}
 
 /* Assign data to their identifier. Data is passed by GNL_iterator
 */
 void	data_assigner(char *data, t_mapdata **map_data)
 {
 	char	**split_data;
-	
+
 	split_data = ft_split(data, ' ');
+	check_identifier(split_data[0]);
 	(void)split_data;
 	(void)map_data;
 }
